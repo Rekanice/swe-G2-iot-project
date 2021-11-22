7cf1ff28d3cc02e3b86ac160b6e29aa314659503
Rekanice <62833151+Rekanice@users.noreply.github.com>
2021-11-22 05:50:20 +0000

    Fix script errors

--- Test/xmas.py
+++ Test/xmas.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+def printlist(ls):
+	for item in ls:
+		print(' - ' + item)
+
 content = []
 src_file_name = 'xmaslist.txt'
 with open(src_file_name, 'r') as f:
@@ -6,8 +10,7 @@ with open(src_file_name, 'r') as f:
 
 print('Hello, Jason.')
 print('Xmas is coming soon. Here is your buy list:\n')
-for item in content:
-	print(' - ' + item)
+printlist(content)
 
 answer = input('Do you want to edit the list? (y/n): ')
 if(answer=='y'):
@@ -16,20 +19,27 @@ if(answer=='y'):
 	if(answer == 'a'):
 		new = input('Enter the new item: ')
 		with open(src_file_name, 'a+') as f:
-			f.append(new)
-			print('Here is the updated list:')
-			print(f.readlines())
+			f.writelines(new)
+			
+		with open(src_file_name, 'r') as f:
+			print('\nHere is the updated list:')
+			printlist(f.readlines())
 
 	if(answer == 'd'):
 		new = input('Enter the to-be-drop item: ')
 		with open(src_file_name, 'r+') as f:
 			content = f.readlines()
-			content.remove(new)
-			f.write(content)
-			print('Here is the updated list:')
-			print(f.readlines())
+			f.seek(0)
+			content.remove(new+'\n')
+			content_stringed = ''.join(content)
+			f.write(content_stringed)
+			f.truncate()
+
+		with open(src_file_name, 'r') as f:
+			print('\nHere is the updated list:')
+			printlist(f.readlines())
 
 elif(answer=='n'):
 	pass
 
-print('\nNow go forth & seize the day! Carpe fookin diem')
+print('\nNow go forth & seize the day! Carpe fookin diem!\n')
