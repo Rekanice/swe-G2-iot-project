3b7af77df1563268e9bd5b83c4bec40bfe961a4d
Rekanice <62833151+Rekanice@users.noreply.github.com>
2022-01-02 11:17:03 +0800

    Trim the working prototype for use case

--- IdleWasher_Flask_API_Server/app.py
+++ IdleWasher_Flask_API_Server/app.py
@@ -10,52 +10,45 @@ logging.basicConfig(level=logging.DEBUG)
 wm_is_idle = 0
 update_time = 0 
 gotLight = 0
-
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/", methods=['GET','POST'])
 def home():
     global wm_is_idle
     global update_time
-    global gotLight
+    # global gotLight
 
     if request.method == 'POST':
-
-        requestContent = request.headers.get('Content-Type')
         
+        global gotLight
+
         # Parse LDR sensor reading
-        if requestContent == 'application/json':
-            app.logger.info(request.json)
-            json = request.get_json()
-            gotLight = json["light"]
-            print("light = " + str(gotLight))
-            print()
-        
-        if requestContent == "application/x-www-form-urlencoded":
-            app.logger.info(request.form)
-            gotLight = request.form.get('light')
-            print("light = " + str(gotLight))
-            print()
-
-        
-        if gotLight:
-            wm_is_idle = 1
-        else:
-            wm_is_idle = 0
+        app.logger.info(request.form)
+        gotLight = request.form.get('light')
+        app.logger.info("gotLight = " + str(gotLight))
+        print()
 
         # Time at which sensor reading is taken
         t = time.localtime()
         update_time = time.strftime("%H:%M", t)
 
+        return render_template(
+        'home.html', 
+        update_time = update_time, 
+        gotLight  = gotLight)
+    
+    # For GET requests, show the global gotLight variable value passed to template
+    app.logger.info("gotLight=" + str(gotLight))
+    print()
 
     return render_template(
         'home.html', 
         update_time = update_time, 
-        wm_is_idle  = wm_is_idle)
+        gotLight  = gotLight)
+
 
 
 if __name__ == '__main__':
     port = os.environ.get("PORT", 5000)              # Get port number of env at runtime, else use default port 5000
     app.run(debug=False, host='0.0.0.0', port=port)  # 0.0.0.0 port forwarding resolves the host IP address at runtime
-    # app.run(debug=False)
