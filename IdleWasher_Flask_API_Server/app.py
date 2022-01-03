6665c0dbfbf4e52cc972ca46e992fc242b687072
Rekanice <62833151+Rekanice@users.noreply.github.com>
2022-01-03 15:56:11 +0800

    Working prototype in localhost & heroku

--- IdleWasher_Flask_API_Server/app.py
+++ IdleWasher_Flask_API_Server/app.py
@@ -11,23 +11,26 @@ logging.basicConfig(level=logging.DEBUG)
 # Replace these variables initial values with the previous values in database later
 gotLight = 0
 update_time = 0 
-
+turbocount = 0
+glcount = 0
 app = Flask(__name__)
 turbo = Turbo(app)
 
 @app.route("/", methods=['GET','POST'])
 def home():
     global gotLight
+    global glcount
 
-    # For POST requests, show the data received
+    # For POST requests, show the data
     if request.method == 'POST':
-        app.logger.info(request.form)  
-        gotLight = request.form.get('light')
+        glcount += 1
+        # app.logger.info(request.form)  
+        gotLight = int(request.form.get('light'))
         print()
         return render_template('home.html')
     
     # For GET requests, show the global gotLight variable value passed to template
-    app.logger.info("During GET request, gotLight=" + str(gotLight)) 
+    # app.logger.info("During GET request, gotLight=" + str(gotLight)) 
     print()
     return render_template('home.html')
 
@@ -35,12 +38,12 @@ def home():
 @app.context_processor
 def injectSensorData():
     global gotLight
-    app.logger.info("injectSensorData ran. Pass gotlight = " + str(gotLight))
+    # app.logger.info("injectSensorData ran. Pass gotlight = " + str(gotLight))
     return dict(gotLight = int(gotLight))
 
 @app.context_processor
 def injectDateTime():
-    app.logger.info("injectDateTime ran")
+    # app.logger.info("injectDateTime ran")
     return dict(update_time = strftime("%Y %b %d, %a %I:%M %p", localtime()) )
 
 
@@ -50,12 +53,17 @@ def before_first_request():
     threading.Thread(target=update_sensor_data).start()
 
 def update_sensor_data():
-    with app.app_context():
-        while True:
-            sleep(5)
-            turbo.push(turbo.replace(render_template('wm_element.html'), 'wm_element'))
+    global turbocount
+    global glcount
 
+    with app.app_context():    
+        while True:
+            sleep(4)
+            app.logger.info("At turbo function #" + str(turbocount) + " now. It sees gotlight " + "#" + str(glcount) +" = " + str(gotLight))
+            turbocount+=1
+            turbo.push(turbo.replace(render_template('wm_element.html'), 'wm_element'))
 
 if __name__ == '__main__':
     port = os.environ.get("PORT", 5000)              # Get port number of env at runtime, else use default port 5000
     app.run(debug=False, host='0.0.0.0', port=port)  # 0.0.0.0 port forwarding resolves the host IP address at runtime
+ 
